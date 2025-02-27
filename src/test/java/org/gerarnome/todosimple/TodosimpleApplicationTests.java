7c0257d702517e950b62d115152fb8165671e276
Vitor Barros <vitornf1221@gmail.com>
2025-02-27 16:08:42 -0300

    First commit

---
+++ src/test/java/org/gerarnome/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gerarnome.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
